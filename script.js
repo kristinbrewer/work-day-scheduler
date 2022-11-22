f9956a7792127fbd035feb2be1e772cf27233564
Kristin Brewer <kr527106@gmail.com>
2022-11-22 17:43:29 -0500

    added document ready and on click for save button script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$( document ).ready(function() {
+    console.log( 'ready!' );
+  });
+
+  var savebutton = $('.saveBtn');
+
+  savebutton.on('click', function () {
+  console.log('saved!');
+
+  //id in the containing time-block as a key to save the user input in local storage
+  });
+  
+
+
+
+
 $(function () {
+
+
+//on click for button 
+//save to local storage 
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
